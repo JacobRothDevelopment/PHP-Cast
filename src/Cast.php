db94b22e8dd16b30722b354b8144dbf823ebfe38
JacobRothDevelopment <rothy784715+github@gmail.com>
2022-04-30 00:10:25 -0400

    adding cast functionality

--- src/Cast.php
+++ src/Cast.php
@@ -2,70 +2,79 @@
 
 namespace PhpCast;
 
+use Exception;
+use ReflectionClass;
+
 class Cast
 {
     /** Cast any data to a different type
      * @param mixed $data
      * @param string $castType ex. `int`, `object`, or some custom class
-     * @return mixed I suggest you use phpdoc `@var` to type hint the returned value 
+     * @return mixed I suggest you use phpdoc `@var` to type hint the 
+     * returned value 
      */
-    public static function cast($data, string $castType)
+    public static function cast(string $castType, $data)
     {
-        print(gettype($data) . " : ");
-        // TODO need to account for 'array' type. not sure how to handle that yet
         switch ($castType) {
+            case "":
+            case "mixed":
+                return $data;
+                break;
             case 'null':
                 return null;
                 break;
+            case 'integer':
             case 'int':
                 return (int)$data;
                 break;
-            case 'float':
-                return (float)$data;
-                break;
             case 'double':
+            case 'float':
                 return (float)$data;
                 break;
             case 'string':
                 return (string)$data;
                 break;
             case 'bool':
+            case 'boolean':
                 return (bool)$data;
                 break;
-            case 'object':
-                return Cast::castToObject($data, 'object');
+            case 'array':
+                return (array)$data;
                 break;
-            default:
-                /* from here it could be 2 things:
-                    1) custom class
-                    2) array of 
-                */
-                // TODO
-                return Cast::castToObject($data, $castType);
+            case 'object':
+            case 'stdClass':
+                return (object)$data;
+                break;
+            default: // assume these are custom types
+                /* force input data to be object. 
+                 * Handling other stuff is dumb and hard work 
+                 * and I don't want to be dumb or do hard work */
+                if (gettype($data) !== "object") $data = (object)$data;
+                return Cast::castToClass($castType, $data);
                 break;
         }
     }
 
-    private static function castToObject($values, string $class)
+    private static function castToClass(string $class, $o)
     {
-        // if casting to generic object, return it casted as generic object
-        if (strtolower($class) === "object") {
-            return (object)$values;
-        }
-
+        $refClass = new ReflectionClass($class);
         $obj = new $class;
-        foreach ($values as $key => $value) {
-            // Currently, we loop over data values, do it the other way. loop over class props
-            // TODO
-            if (property_exists($class, $key)) {
-                try {
-                    $obj->$key = $value;
-                } catch (\TypeError $e) {
-                    throw new \Exception("TODO"); // TODO
-                }
+        $refProps = $refClass->getProperties();
+        foreach ($refProps as $refProp) {
+            $propName = $refProp->getName();
+            $propType = $refProp->getType();
+
+            // if class property doesn't have a type; treat it as 'mixed'
+            $propTypeName = "";
+            if ($propType !== null) $propTypeName = $propType->getName();
+
+            print("$propTypeName $propName \r\n");
+            if (isset($o->$propName)) {
+                $obj->$propName = Cast::cast($propTypeName, $o->$propName);
+            } else {
+                $obj->$propName = null;
             }
         }
-
         return $obj;
     }
 }
